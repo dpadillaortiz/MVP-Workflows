7b96b6efa8b6ebddff7a91e5a3cad5f56de30d4a
twinky <padillaortiz.daniel099@gmail.com>
2025-11-09 21:40:48 -0800

    tadded folder structure

---
+++ terraform/tenant/main.tf
@@ -0,0 +1,30 @@
+variable "auth0_domain" {
+  description = "Auth0 tenant domain"
+  type        = string
+}
+
+variable "auth0_client_id" {
+  description = "Auth0 client ID"
+  type        = string
+}
+
+variable "auth0_client_secret" {
+  description = "Auth0 client secret"
+  type        = string
+  sensitive   = true
+}
+
+terraform {
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "auth0" {
+  domain        = var.auth0_domain
+  client_id     = var.auth0_client_id
+  client_secret = var.auth0_client_secret
+}
