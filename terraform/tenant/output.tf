27fe9bd9b7f9805835dad270a458f6a410705887
twinky <padillaortiz.daniel099@gmail.com>
2025-11-12 21:27:38 -0800

    fixing  Error: Reference to undeclared resource from github action

--- terraform/tenant/output.tf
+++ terraform/tenant/output.tf
@@ -1,5 +1,5 @@
 output "output_name" {
-  value       = resource.auth0_client.my_client.id # or resource.type.name.attribute
+  value       = resource.auth0_client.grafana.id # or resource.type.name.attribute
   description = "A description of the output value."
   #sensitive = true
 }
